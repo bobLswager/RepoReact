e73523776f645cc8f926a8b9b9cb11bbf9b8bb0b
Gabriel Ortega <o.gabriel18@hotmail.com>
2024-03-04 21:08:25 -0300

    primer commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './App.css'
+import Button from '../Button/Button'
+import { useState } from 'react';
+import Pages from '../MostrarPagina/Pages';
+
+
+export const userContext = React.createContext();
+
+function App() {
+
+  const [pagina , setPagina ] = useState(null);
+  
+  return (
+    <>
+    
+      <div className='card'>
+        <div class="circle"></div>
+        <div class="circle"></div>
+        <div class="card-inner">
+          <div className='divImg'><img src="src\assets\img1.png" alt="" /></div>
+          <Button  setPagina={setPagina}/>
+          <div className='pages'></div>
+        </div>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
+
---
+++ src/components/MostrarPagina/Pages.jsx
@@ -0,0 +1,19 @@
+import './paginas.css'
+
+function Pages(){
+
+const content = () => { 
+    
+}
+
+
+    return(
+        <>
+        <div>
+        {content}
+        </div>
+        </>
+    )
+}
+
+export default Pages
